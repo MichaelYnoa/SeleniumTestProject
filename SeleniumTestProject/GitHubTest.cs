3f5511d99d0247f1fd777864421436ded85138ec
Lenovo_MAY <michaelaraujoynoa@gmail.com>
2022-05-22 18:50:57 -0400

    Add project files.

---
+++ SeleniumTestProject/GitHubTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+public class GitHubTest
+{
+
+    private const string SearchPhrase = "selenium";
+
+    private static IWebDriver driver;
+
+    [OneTimeSetUp]
+    public static void SetUpDriver()
+    {
+        new DriverManager().SetUpDriver(new ChromeConfig());
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+    }
+
+    [Test]
+
+    public void CheckGitHubSearch()
+    {
+
+        
+
+        driver.Navigate().GoToUrl("https://www.github.com");
+
+        
+
+        IWebElement searchInput = driver.FindElement(By.CssSelector("[name='q']"));
+        searchInput.SendKeys(SearchPhrase);
+        searchInput.SendKeys(Keys.Enter);
+
+        IList<string> actualItems = driver.FindElements(By.CssSelector(".repo-list-item"))
+            .Select(item => item.Text.ToLower())
+            .ToList();
+        IList<string> expectedItems = actualItems
+            .Where(item => item.Contains("Invalid search phrase"))
+            .ToList();
+
+        Assert.AreEqual(expectedItems, actualItems);
+
+        
+    }
+
+    [OneTimeTearDown]
+    public static void TearDownDriver()
+    {
+        driver.Quit();
+    }
+}
